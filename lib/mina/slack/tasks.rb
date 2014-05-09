db30b43104df954b47ec6f52f65498ed80ea5033
Jeremy Mack <jeremy@pileofturtles.com>
2014-05-09 12:20:12 -0400

    feat: link to github commit and DRY message

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -5,20 +5,33 @@ require 'net/http'
 
 require 'mina/slack/defaults'
 
-
-
 # Before and after hooks for mina deploy
 before_mina :deploy, :'slack:starting'
 after_mina :deploy, :'slack:finished'
 
+def message(options)
+  short_hash = options[:commit_hash][0..7]
+  commit_link = "<#{options[:github_url]}/commit/"+
+    "#{options[:commit_hash]}|#{options[:branch]}@#{short_hash}>"
+
+  "#{options[:deployer]} #{options[:verb]} #{options[:app]}'s"+
+    " #{commit_link} to #{options[:environment]}"
+end
 
 # Slack tasks
 namespace :slack do
 
   task :starting do
     if slack_token and slack_room and slack_subdomain
-      announced_stage = environment || 'production'
-      announcement = "#{deployer} is deploying #{app}'s #{branch} branch to #{announced_stage}"
+      message = message(
+        verb: 'is deploying',
+        github_url: github_url,
+        commit_hash: commit_hash,
+        app: app,
+        deployer: deployer,
+        environment: environment,
+        branch: branch
+      )
 
       # Parse the API url and create an SSL connection
       uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
@@ -28,10 +41,10 @@ namespace :slack do
 
       # Create the post request and setup the form data
       request = Net::HTTP::Post.new(uri.request_uri)
-      request.set_form_data(payload: {channel: slack_room, username: 'deploybot', text: announcement, icon_emoji: ':ghost:'}.to_json)
+      request.set_form_data(payload: {channel: slack_room, username: 'deploybot', text: message, icon_emoji: ':ghost:'}.to_json)
 
       # Make the actual request to the API
-      response = http.request(request)
+      http.request(request)
     else
       print_local_status "Unable to create Slack Announcement, no slack details provided."
     end
@@ -41,8 +54,15 @@ namespace :slack do
 
   task :finished do
     if slack_token and slack_room and slack_subdomain
-      announced_stage = environment || 'production'
-      announcement = "#{deployer} successfully deployed #{app}'s #{branch} branch to #{announced_stage}!"
+      message = message(
+        verb: 'deployed',
+        github_url: github_url,
+        commit_hash: commit_hash,
+        app: app,
+        deployer: deployer,
+        environment: environment,
+        branch: branch
+      )
 
       # Parse the URI and handle the https connection
       uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
@@ -52,10 +72,10 @@ namespace :slack do
 
       # Create the post request and setup the form data
       request = Net::HTTP::Post.new(uri.request_uri)
-      request.set_form_data(payload: {channel: slack_room, username: 'deploybot', text: announcement, icon_emoji: ':ghost:'}.to_json)
+      request.set_form_data(payload: {channel: slack_room, username: 'deploybot', text: message, icon_emoji: ':ghost:'}.to_json)
 
       # Make the actual request to the API
-      response = http.request(request)
+      http.request(request)
     else
       print_local_status "Unable to create Slack Announcement, no slack details provided."
     end
